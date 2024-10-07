788e7f9682d8e29c788c3e401a14a7bceea6fdc2
Dattb0i <doea@wlhs.wlwv.k12.or.us>
2024-10-07 16:31:44 +0000

    madlib

---
+++ madlib.py
@@ -0,0 +1,29 @@
+'''
+Name: August Doe
+Date: 10/7/24
+Description: Unit 2 HW 2
+'''
+
+name = input("What's your name")
+date = input("What's the date today?")
+
+noun1 = input("pick a noun")
+noun2 = input("pick another")
+noun3 = input("and another")
+noun4 = input("another")
+noun5 = input("another")
+noun6 = input("ok last one")
+adverb1 = input("pick an adverb")
+abverb2 = input("one more")
+verb1 = input("pick a verb")
+verb2 = input("pick another")
+verb3 = input("pick another")
+verb4 = input("another")
+verb5 = input("alright that's it")
+adjective1 = input("pick an adjective")
+adjective2 = input("pick another")
+adjective3 = input("one more")
+
+print(f"your name is {name.title()}")
+print(f"today's date is {date}")
+print(f"{adverb1}, there was a {noun1}{verb1}on a {noun2}. It was {adjective1}outside, and the {noun1}was too {adjective2}to go outside. Then, the {noun1}'s pet {noun3}{verb2}up and started {verb3}itself, which{adjective3}{noun1}. Some grilled {noun4}was being{verb4}, and everyone {adverb2}town wanted some of it, including the local {noun5}. In the end, everybody got some, even the {verb5}{noun6}.")
